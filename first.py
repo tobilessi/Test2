ef3a9b2c1a7dfcad93406108fbd74e410224da6f
agnimel <agnimel@localhost.localdomain>
2024-02-02 06:43:46 +0000

    mon_commit

---
+++ first.py
@@ -0,0 +1,8 @@
+NUM = int(input("Le numérateur"))
+DEN = 0
+while(DEN==0):
+    print("Entrer DEN >0")
+    DEN = int(input("Veuillez entrez DEN"))
+QUO = NUM / DEN 
+print("LE QUOTIENT EST :" ,QUO)
+    
